428eda4c559684fe8a9e7084faf48f4119ea1a68
kingmocs <kingalexxcvi96@gmail.com>
2020-08-09 14:51:12 -0700

    hover

--- script.js
+++ script.js
@@ -107,3 +107,29 @@ function fadeIn(card_id, opacity){
     opacity += 0.1;
   }, 50);
 }
+
+function watchForHover() {
+  // lastTouchTime is used for ignoring emulated mousemove events
+  let lastTouchTime = 0
+
+  function enableHover() {
+    if (new Date() - lastTouchTime < 500) return
+    document.body.classList.add('hasHover')
+  }
+
+  function disableHover() {
+    document.body.classList.remove('hasHover')
+  }
+
+  function updateLastTouchTime() {
+    lastTouchTime = new Date()
+  }
+
+  document.addEventListener('touchstart', updateLastTouchTime, true)
+  document.addEventListener('touchstart', disableHover, true)
+  document.addEventListener('mousemove', enableHover, true)
+
+  enableHover()
+}
+
+watchForHover()
